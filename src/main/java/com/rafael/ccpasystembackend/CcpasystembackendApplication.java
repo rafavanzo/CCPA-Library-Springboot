fc723e713a5d716bee2855ed8373c192044f4795
Rafael Vanzo <rafacvanzo@hotmail.com>
2024-05-18 19:40:31 -0300

    Commit Inicial

---
+++ src/main/java/com/rafael/ccpasystembackend/CcpasystembackendApplication.java
@@ -0,0 +1,13 @@
+package com.rafael.ccpasystembackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcpasystembackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcpasystembackendApplication.class, args);
+	}
+
+}
