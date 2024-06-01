fc046af0693ad68a080d87749727695d4c6f2449
Rafael Vanzo <rafacvanzo@hotmail.com>
2024-05-31 21:35:40 -0300

    HTTP POST

--- src/main/java/com/rafael/ccpasystembackend/controller/LivrosController.java
+++ src/main/java/com/rafael/ccpasystembackend/controller/LivrosController.java
@@ -2,8 +2,13 @@ package com.rafael.ccpasystembackend.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rafael.ccpasystembackend.model.Livro;
@@ -24,5 +29,10 @@ public class LivrosController {
         return livroRepository.findAll();
     }
 
+    @PostMapping
+    public ResponseEntity<Livro> create(@RequestBody Livro livro) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(livroRepository.save(livro));
+    }
 
 }
