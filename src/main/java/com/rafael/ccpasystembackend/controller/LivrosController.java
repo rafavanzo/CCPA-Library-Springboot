4d362942d6bcaf8ac9584c6bfa25bc43e92eb128
Rafael Vanzo <rafacvanzo@hotmail.com>
2024-06-01 18:15:45 -0300

    API Spring: Buscar por ID

--- src/main/java/com/rafael/ccpasystembackend/controller/LivrosController.java
+++ src/main/java/com/rafael/ccpasystembackend/controller/LivrosController.java
@@ -5,10 +5,10 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rafael.ccpasystembackend.model.Livro;
@@ -29,10 +29,16 @@ public class LivrosController {
         return livroRepository.findAll();
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Livro> getById(@PathVariable Long id) {
+        return livroRepository.findById(id)
+                .map(result -> ResponseEntity.ok().body(result))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
     @PostMapping
     public ResponseEntity<Livro> create(@RequestBody Livro livro) {
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(livroRepository.save(livro));
     }
-
 }
