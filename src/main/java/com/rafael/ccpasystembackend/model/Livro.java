9e7c1eaae9b2bed724c168f8fb20b18afa36c5ac
Rafael Vanzo <rafacvanzo@hotmail.com>
2024-06-02 21:31:00 -0300

    Atualiza tabela Livro

--- src/main/java/com/rafael/ccpasystembackend/model/Livro.java
+++ src/main/java/com/rafael/ccpasystembackend/model/Livro.java
@@ -1,5 +1,10 @@
 package com.rafael.ccpasystembackend.model;
 
+import java.sql.Timestamp;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.persistence.Column;
@@ -13,28 +18,36 @@ import lombok.Data;
 @Entity
 //@Table(name = "livros")
 public class Livro {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(nullable = false, updatable = false, unique = true)
     @JsonProperty("_idLivro")
     private Long idLivro;
-    
-    @Column(nullable = false, unique = true)
+
+    @Column(nullable = false, unique = true, length = 255)
     private String codigo;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 100)
     private String titulo;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 100)
     private String editora;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 10)
     private String dataLancamento;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 50)
     private String autor;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 50)
     private String genero;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @CreationTimestamp
+    private Timestamp createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    @UpdateTimestamp
+    private Timestamp updatedAt;
 }
